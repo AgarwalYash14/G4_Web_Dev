6f9492331f24a966e1fdc8d8ee9ce29f09389be7
YashAgarwal <agarwal.yash14@hotmail.com>
2025-08-07 14:14:40 +0530

    L21

---
+++ L21-DEV_COMMUNITY_JWT/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { registerUser, loginUser } = require('../controllers/userController')
+
+const router = express.Router()
+
+const app = express()
+
+app.use(function (req, res, next) {
+    console.log('THIS IS ROUTER LEVEL MIDDLEWARE')
+    next()
+})
+
+router.post('/register', registerUser)
+router.post('/login', loginUser)
+
+module.exports = router
